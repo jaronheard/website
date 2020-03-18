e251554be4b54b56ba6aa2feecfd5edd07cae247
Dianna Faulk <diannafaulk@gmail.com>
2020-03-18 15:35:20 -0700

    Abstract trio design into component

---
+++ src/components/GridTrio.js
@@ -0,0 +1,80 @@
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import { PropTypes } from "prop-types";
+
+import DividerLine from "./DividerLine";
+import CallToActionCard from "./CallToActionCard";
+import { smBreak } from "../_Theme/UpdatedBrandTheme";
+
+const GridTrio = ({
+  title,
+  subtitle,
+  callToActionBlockList,
+  showDividerLine,
+  dividerLineColor
+}) => {
+  return (
+    <div>
+      <div
+        css={css`
+          width: 100%;
+          background-color: white;
+          padding-top: 100px;
+          ${smBreak} {
+            padding-top: 0;
+          }
+        `}
+      >
+        <div
+          css={css`
+            max-width: 600px;
+            display: grid;
+            justify-items: center;
+            margin: 0 auto;
+          `}
+        >
+          {title && <h2>{title}</h2>}
+          {subtitle && <p>{subtitle}</p>}
+        </div>
+      </div>
+      <div
+        css={css`
+          display: grid;
+          margin: 0 auto;
+          grid-template-columns: repeat(3, 1fr);
+          padding: 0 40px;
+          margin: 25px auto;
+          max-width: 1330px;
+
+          @media (max-width: 1230px) {
+            width: min-content;
+            justify-items: center;
+            grid-template-columns: repeat(1, 1fr);
+            grid-template-rows: 1fr 1fr 1fr;
+            grid-row-gap: 20px;
+            width: auto;
+          }
+
+          ${smBreak} {
+            padding: 0 10px;
+          }
+        `}
+      >
+        {callToActionBlockList.map(({ summary, tagline }) => {
+          return <CallToActionCard tagline={tagline} summary={summary.json} />;
+        })}
+      </div>
+      {showDividerLine && <DividerLine hexColor={dividerLineColor} swoopUp />}
+    </div>
+  );
+};
+
+GridTrio.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  callToActionBlockList: PropTypes.arrayOf(PropTypes.shape({})),
+  showDividerLine: PropTypes.bool,
+  dividerLineColor: PropTypes.string
+};
+
+export default GridTrio;
--- src/pages/DisciplinesAndRoles/CivicExperience.js
+++ src/pages/DisciplinesAndRoles/CivicExperience.js
@@ -1,11 +1,10 @@
 /* eslint-disable graphql/template-strings */
 /** @jsx jsx */
-import { jsx, css } from "@emotion/core";
+import { jsx } from "@emotion/core";
 import { useStaticQuery, graphql } from "gatsby";
 
-import DividerLine from "../../components/DividerLine";
-import CallToActionCard from "../../components/CallToActionCard";
-import { smBreak, colors } from "../../_Theme/UpdatedBrandTheme";
+import { colors } from "../../_Theme/UpdatedBrandTheme";
+import GridTrio from "../../components/GridTrio";
 
 const CivicExperience = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -29,58 +28,13 @@ const CivicExperience = () => {
   );
 
   return (
-    <div>
-      <div
-        css={css`
-          width: 100%;
-          background-color: white;
-          padding-top: 100px;
-          ${smBreak} {
-            padding-top: 0;
-          }
-        `}
-      >
-        <div
-          css={css`
-            max-width: 600px;
-            display: grid;
-            justify-items: center;
-            margin: 0 auto;
-          `}
-        >
-          <h2>{contentfulContentList.title}</h2>
-          <p>{contentfulContentList.subtitle.subtitle}</p>
-        </div>
-      </div>
-      <div
-        css={css`
-          display: grid;
-          margin: 0 auto;
-          grid-template-columns: repeat(3, 1fr);
-          padding: 0 40px;
-          margin: 25px auto;
-          max-width: 1330px;
-
-          @media (max-width: 1230px) {
-            width: min-content;
-            justify-items: center;
-            grid-template-columns: repeat(1, 1fr);
-            grid-template-rows: 1fr 1fr 1fr;
-            grid-row-gap: 20px;
-            width: auto;
-          }
-
-          ${smBreak} {
-            padding: 0 10px;
-          }
-        `}
-      >
-        {contentfulContentList.content.map(({ summary, tagline }) => {
-          return <CallToActionCard tagline={tagline} summary={summary.json} />;
-        })}
-      </div>
-      <DividerLine hexColor={colors.green.hex} swoopUp />
-    </div>
+    <GridTrio
+      title={contentfulContentList.title}
+      subtitle={contentfulContentList.subtitle.subtitle}
+      callToActionBlockList={contentfulContentList.content}
+      showDividerLine
+      dividerLineColor={colors.green.hex}
+    />
   );
 };
 
