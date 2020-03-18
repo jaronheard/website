ed7f46950a90e795d4c13664c7d8fed33ff63a1e
Dianna Faulk <diannafaulk@gmail.com>
2020-03-18 16:02:04 -0700

    Abstract philophy section

---
+++ src/pages/DisciplinesAndRoles/Philosophy.js
@@ -0,0 +1,61 @@
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import { useStaticQuery, graphql } from "gatsby";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+
+import { smBreak, xsBreak } from "../../_Theme/UpdatedBrandTheme";
+
+const disciplinesAndRoles = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { contentfulCallToActionBlock } = useStaticQuery(
+    graphql`
+      query {
+        contentfulCallToActionBlock(
+          contentful_id: { eq: "74dAGTiSRBzwL5yXbUuWY0" }
+        ) {
+          tagline
+          summary {
+            json
+          }
+        }
+      }
+    `
+  );
+
+  return (
+    <div
+      css={css`
+        width: 100%;
+        background-color: white;
+        padding-top: 100px;
+        ${smBreak} {
+          padding-top: 0;
+        }
+      `}
+    >
+      <div
+        css={css`
+          max-width: 600px;
+          display: grid;
+          justify-items: center;
+          margin: 0 auto;
+
+          ${smBreak} {
+            padding: 0 40px;
+          }
+
+          ${xsBreak} {
+            padding: 0 20px;
+          }
+        `}
+      >
+        <h2>{contentfulCallToActionBlock.tagline}</h2>
+        <p>
+          {documentToReactComponents(contentfulCallToActionBlock.summary.json)}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default disciplinesAndRoles;
--- src/pages/disciplines-and-roles.js
+++ src/pages/disciplines-and-roles.js
@@ -1,33 +1,25 @@
 /** @jsx jsx */
-import { jsx, css } from "@emotion/core";
+import { jsx } from "@emotion/core";
 import { useStaticQuery, graphql } from "gatsby";
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 import PageLayout from "../components/PageLayout";
 import TitleArea from "../components/TitleArea";
 import DividerLine from "../components/DividerLine";
 import scatterplotImage from "../images/scatterplot.png";
-import { colors, smBreak, xsBreak } from "../_Theme/UpdatedBrandTheme";
+import { colors } from "../_Theme/UpdatedBrandTheme";
+import Philosophy from "./DisciplinesAndRoles/Philosophy";
 import DeSiloExperience from "./DisciplinesAndRoles/DeSiloExperience";
 import CivicExperience from "./DisciplinesAndRoles/CivicExperience";
 
 const disciplinesAndRoles = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const { contentfulHeading, contentfulCallToActionBlock } = useStaticQuery(
+  const { contentfulHeading } = useStaticQuery(
     graphql`
       query {
         contentfulHeading(contentful_id: { eq: "4NCEgZyiEd8QQnm3njhaw4" }) {
           id
           title
           subtitle
-        }
-        contentfulCallToActionBlock(
-          contentful_id: { eq: "74dAGTiSRBzwL5yXbUuWY0" }
-        ) {
-          tagline
-          summary {
-            json
-          }
         }
       }
     `
@@ -45,40 +37,7 @@ const disciplinesAndRoles = () => {
         subtitle={contentfulHeading.subtitle}
         swooshColor={colors.green.hex}
       />
-      <div
-        css={css`
-          width: 100%;
-          background-color: white;
-          padding-top: 100px;
-          ${smBreak} {
-            padding-top: 0;
-          }
-        `}
-      >
-        <div
-          css={css`
-            max-width: 600px;
-            display: grid;
-            justify-items: center;
-            margin: 0 auto;
-
-            ${smBreak} {
-              padding: 0 40px;
-            }
-
-            ${xsBreak} {
-              padding: 0 20px;
-            }
-          `}
-        >
-          <h2>{contentfulCallToActionBlock.tagline}</h2>
-          <p>
-            {documentToReactComponents(
-              contentfulCallToActionBlock.summary.json
-            )}
-          </p>
-        </div>
-      </div>
+      <Philosophy />
       <DividerLine hexColor={colors.blue.hex} swoopUp />
       <DeSiloExperience />
       <CivicExperience />
