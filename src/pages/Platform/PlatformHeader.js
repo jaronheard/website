92d8fb7111fce555ded5c7168c164b23a0f1cf88
Dianna Faulk <diannafaulk@gmail.com>
2020-03-18 21:15:24 -0700

    Update the Platform title area

--- src/pages/Platform/PlatformHeader.js
+++ src/pages/Platform/PlatformHeader.js
@@ -1,28 +1,31 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
-import PageHeading from "../../components/PageHeading";
+
+import scatterplotImage from "../../images/scatterplot.png";
+import { colors } from "../../_Theme/UpdatedBrandTheme";
+import TitleArea from "../../components/TitleArea";
 
 const PlatformHeader = () => {
-  const { allContentfulHeading } = useStaticQuery(
+  const { contentfulHeading } = useStaticQuery(
     graphql`
       query {
-        allContentfulHeading(
-          filter: { contentful_id: { eq: "5e9eWbUqWY0lhyF9TRiSmh" } }
-        ) {
-          edges {
-            node {
-              title
-              subtitle
-            }
-          }
+        contentfulHeading(contentful_id: { eq: "5e9eWbUqWY0lhyF9TRiSmh" }) {
+          title
+          subtitle
         }
       }
     `
   );
 
-  return allContentfulHeading.edges.map(el => (
-    <PageHeading title={el.node.title} subtitle={el.node.subtitle} />
-  ));
+  return (
+    <TitleArea
+      imageURL={scatterplotImage}
+      imageOpacity={1}
+      title={contentfulHeading.title}
+      subtitle={contentfulHeading.subtitle}
+      swooshColor={colors.purple.hex}
+    />
+  );
 };
 
 export default PlatformHeader;
--- src/pages/platform.js
+++ src/pages/platform.js
@@ -1,9 +1,8 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
-import { Grid } from "@material-ui/core";
 
 import PlatformHeader from "./Platform/PlatformHeader";
-import ContributorBlock from "./Platform/ContributorBlock";
+// import ContributorBlock from "./Platform/ContributorBlock";
 import PageLayout from "../components/PageLayout";
 import { colors } from "../_Theme/UpdatedBrandTheme";
 
@@ -15,12 +14,8 @@ const PlatformPage = () => {
       swoopUp
       swoopColor={colors.purple.hex}
     >
-      <Grid container spacing={0}>
-        <PlatformHeader />
-        <Grid container xs={12} spacing={4}>
-          <ContributorBlock />
-        </Grid>
-      </Grid>
+      <PlatformHeader />
+      {/* <ContributorBlock /> */}
     </PageLayout>
   );
 };
