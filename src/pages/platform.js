9ebab4accaa32cf637f3944fc1e00c16787ac6c1
Dianna Faulk <diannafaulk@gmail.com>
2020-03-18 21:07:09 -0700

    Make swoop under common CTA stylable

--- src/pages/platform.js
+++ src/pages/platform.js
@@ -5,10 +5,16 @@ import { Grid } from "@material-ui/core";
 import PlatformHeader from "./Platform/PlatformHeader";
 import ContributorBlock from "./Platform/ContributorBlock";
 import PageLayout from "../components/PageLayout";
+import { colors } from "../_Theme/UpdatedBrandTheme";
 
 const PlatformPage = () => {
   return (
-    <PageLayout title="Platform">
+    <PageLayout
+      title="Platform"
+      keywords={[`Civic Software Foundation`, `CIVIC Platform`]}
+      swoopUp
+      swoopColor={colors.purple.hex}
+    >
       <Grid container spacing={0}>
         <PlatformHeader />
         <Grid container xs={12} spacing={4}>
--- src/pages/ventures.js
+++ src/pages/ventures.js
@@ -6,10 +6,11 @@ import VenturesHeader from "./Ventures/VenturesHeader";
 import ContributorBlock from "./Ventures/ContributorBlock";
 import PageLayout from "../components/PageLayout";
 import EmphasizedCTA from "./Ventures/EmphasizedCTA";
+import { colors } from "../_Theme/UpdatedBrandTheme";
 
 const VenturesPage = () => {
   return (
-    <PageLayout title="Ventures">
+    <PageLayout title="Ventures" swoopUp swoopColor={colors.purple.hex}>
       <Grid container spacing={0}>
         <VenturesHeader />
         <Grid container xs={12} spacing={4}>
