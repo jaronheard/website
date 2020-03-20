e9479ff9ffead3e4a1c3b059b14c8b0273e066ab
Jaron Heard <jaronheard@gmail.com>
2020-03-19 19:53:51 -0700

    Project page (#8)
    * Factor out ShadowBox
    
    * update projects page
    
    * update schema
    
    * updated projects page

---
+++ src/components/ShadowBox.js
@@ -0,0 +1,67 @@
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import PropTypes from "prop-types";
+
+const ShadowBox = ({
+  title,
+  MainContent,
+  FooterContent,
+  Button,
+  cardStyle,
+  shadowColor,
+  wide
+}) => {
+  return (
+    <div
+      className="ShadowBox"
+      css={css`
+        ${wide || FooterContent ? `max-width: 400px;` : ""}
+        ${shadowColor ? `box-shadow: 6px 6px 0px rgb(${shadowColor});` : ""}
+        ${cardStyle}
+      `}
+    >
+      <div className="ShadowBoxContent">
+        {title && (
+          <h3
+            css={css`
+              text-align: center;
+              margin-top: 0;
+            `}
+          >
+            {title}
+          </h3>
+        )}
+        {MainContent}
+        {Button}
+      </div>
+      {FooterContent && (
+        <div
+          className="ShadowBoxFooter"
+          css={css`
+            ${shadowColor
+              ? `
+            > ul {
+              background-color: rgba(${shadowColor}, .1);
+            }
+          `
+              : ""}
+          `}
+        >
+          {FooterContent}
+        </div>
+      )}
+    </div>
+  );
+};
+
+ShadowBox.propTypes = {
+  title: PropTypes.string,
+  MainContent: PropTypes.node,
+  FooterContent: PropTypes.node,
+  Button: PropTypes.node,
+  cardStyle: PropTypes.string, // css
+  shadowColor: PropTypes.string, // hex code
+  wide: PropTypes.bool
+};
+
+export default ShadowBox;
