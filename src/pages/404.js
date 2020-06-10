e1a0abbea06a5a9c8417d396f3e854bf8bbbeab0
Jaron Heard <jaronheard@gmail.com>
2020-06-10 02:47:38 -0400

    add layout fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,13 @@
 import React from "react";
 
 import SEO from "../components/SEO";
-import Wrapper from "../components/Wrapper";
 
 const NotFoundPage = () => (
-  <Wrapper>
-    <div heroTitle="Civic and Gatsby Starter" heroSubtitle="They play nice!">
-      <SEO title="404: Not found" />
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
-  </Wrapper>
+  <div heroTitle="Civic and Gatsby Starter" heroSubtitle="They play nice!">
+    <SEO title="404: Not found" />
+    <h1>NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </div>
 );
 
 export default NotFoundPage;
